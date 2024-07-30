6d7aea73f3aed7f0ea993146a2743de206bd44b2
hiba rassas <hibarassas12l@gmail.com>
2024-07-30 18:20:21 +0200

    v2

--- client-main/src/pages/ContactPage.jsx
+++ client-main/src/pages/ContactPage.jsx
@@ -96,7 +96,7 @@ const PhoneInputWrapper = styled.div`
         background-color: #f5f5f5;
         width: 150px;
         text-align: center;
-        height: fit-content;
+        
     }
 
     .country-code {
